627d103ce78b72c142290df4b883156acf292c5f
jinnymolba <133872166+petitjinnmolba@users.noreply.github.com>
2025-05-19 09:55:07 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt() + s.nextInt());
+    }
+}

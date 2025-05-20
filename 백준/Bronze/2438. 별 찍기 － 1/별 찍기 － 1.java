493ea7536cc528baba73ce259066e9d27060ea24
jinnymolba <133872166+petitjinnmolba@users.noreply.github.com>
2025-05-20 18:04:46 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 252 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        
+        for(int a = 1; a<= N; a++){
+            for(int b = 1; b<= a; b++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}

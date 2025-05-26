318d5badbdba982eab379c887384e460b30f467f
jinnymolba <133872166+petitjinnmolba@users.noreply.github.com>
2025-05-26 15:55:37 +0900

    [Bronze V] Title: A+B - 5, Time: 184 ms, Memory: 17844 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        while (true){
+            int A = s.nextInt();
+            int B = s.nextInt();
+            
+            if (A == 0 && B == 0){
+                break;
+            }
+            
+            System.out.println(A + B);
+        }
+        s.close();
+    }
+}

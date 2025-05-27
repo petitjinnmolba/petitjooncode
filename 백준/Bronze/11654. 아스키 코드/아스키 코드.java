49d59d2e628f40e517bb821c98c4589ca43d3ccd
jinnymolba <133872166+petitjinnmolba@users.noreply.github.com>
2025-05-27 20:39:10 +0900

    [Bronze V] Title: 아스키 코드, Time: 188 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        char c = s.next().charAt(0);
+        System.out.println((int) c);
+    }
+}
